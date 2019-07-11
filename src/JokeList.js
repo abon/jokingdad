0fb6547b5df52c878bc1679851cfa730a4f44a87
Nasafiy <formygithubacc@gmail.com>
2019-07-11 23:21:07 +0500

    API & sidebar fixes

--- src/JokeList.js
+++ src/JokeList.js
@@ -26,10 +26,7 @@ export default class JokeList extends Component {
     return (
       <div className="JokeList">
         <div className="JokeList-sidebar">
-          <h1 className="JokeList-title">
-            {" "}
-            <span> Joking</span> Dad
-          </h1>
+          <h1 className="JokeList-title"> Joking Dad</h1>
           <img
             src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
             alt=""
