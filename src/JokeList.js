9dbde2ed88555c466878470feb3f568fcf9a8e97
Nasafiy <formygithubacc@gmail.com>
2019-07-12 01:50:25 +0500

    vote's css & voting login added

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import Joke from "./Joke";
 import axios from "axios";
+import uuid from "uuid/v4";
 import "./JokeList.css";
 export default class JokeList extends Component {
   static defaultProps = {
@@ -16,12 +18,18 @@ export default class JokeList extends Component {
       let res = await axios.get("https://icanhazdadjoke.com", {
         headers: { Accept: "application/json" }
       });
-      jokes.push(res.data.joke);
+      jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
     }
     this.setState({ jokes: jokes });
     console.log(jokes);
   }
-
+  handleVote(id, delta) {
+    this.setState(st => ({
+      jokes: st.jokes.map(j =>
+        j.id === id ? { ...j, votes: j.votes + delta } : j
+      )
+    }));
+  }
   render() {
     return (
       <div className="JokeList">
@@ -35,7 +43,15 @@ export default class JokeList extends Component {
         </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map(j => (
-            <div>{j}</div>
+            <div>
+              <Joke
+                key={j.id}
+                votes={j.votes}
+                text={j.text}
+                downvote={() => this.handleVote(j.id, -1)}
+                upvote={() => this.handleVote(j.id, 1)}
+              />
+            </div>
           ))}
         </div>
       </div>
